02d9e3bc7298051c1e3676db0cfb0a59edf3d81d
Larkshiro <23059958+tdt188@users.noreply.github.com>
2022-10-27 08:19:09 +0000

    Update aaaa.sh

--- aaaa.sh
+++ aaaa.sh
@@ -84,7 +84,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//$IP4/$port/$(gen64 $IP6)"
+        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
